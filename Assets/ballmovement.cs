17e4b4c2ff4173b4aa7f38e141d9c63cac52a438
atikatsmr <atikatsamara72@gmail.com>
2020-04-02 21:40:39 +0700

    4th commit

--- Assets/ballmovement.cs
+++ Assets/ballmovement.cs
@@ -6,16 +6,23 @@ public class ballmovement : MonoBehaviour
 {
     public int speed=30;
     public Rigidbody2D sesuatu;
+
+    public Animator animtr;
     // Start is called before the first frame update
     void Start()
     {
         sesuatu.velocity = new Vector2(-1,-1) * speed;
+        animtr.SetBool("isMove", true);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(sesuatu.velocity.x > 0){ //bola bergerak kekanan
+            sesuatu.GetComponent<Transform>().localScale = new Vector3(1,1,1);
+        }else{
+            sesuatu.GetComponent<Transform>().localScale = new Vector3(-1,1,1);
+        }
     }
 
     void OnCollisionEnter2D(Collision2D other) {
@@ -25,8 +32,10 @@ public class ballmovement : MonoBehaviour
     }
     IEnumerator jeda(){
         sesuatu.velocity = new Vector2(0,0);
+        animtr.SetBool("isMove", false);
         sesuatu.GetComponent<Transform>().position = new Vector2(0,0);
         yield return new WaitForSeconds(1);
         sesuatu.velocity = new Vector2(-1,-1) * speed;
+        animtr.SetBool("isMove", true);
     }
 }
